e4dc286e67d473cf8410b49381a2d9de60d58167
KristelEiTea <kristel.siilak@hotmail.com>
2018-01-09 09:49:04 +0200

    nupp

--- Triangle/Tehnika.Designer.cs
+++ Triangle/Tehnika.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.Kõrvaklapid_4 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
@@ -44,14 +44,14 @@
             this.button14 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // Kõrvaklapid_4
             // 
-            this.button1.Location = new System.Drawing.Point(81, 122);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 42);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Kõrvaklapid_4.Location = new System.Drawing.Point(81, 122);
+            this.Kõrvaklapid_4.Name = "Kõrvaklapid_4";
+            this.Kõrvaklapid_4.Size = new System.Drawing.Size(110, 42);
+            this.Kõrvaklapid_4.TabIndex = 0;
+            this.Kõrvaklapid_4.Text = "Nööp kõrvaklapid\r\n   4€";
+            this.Kõrvaklapid_4.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
@@ -188,7 +188,7 @@
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Kõrvaklapid_4);
             this.Name = "Tehnika";
             this.Text = "Tehnika";
             this.ResumeLayout(false);
@@ -197,7 +197,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Kõrvaklapid_4;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
